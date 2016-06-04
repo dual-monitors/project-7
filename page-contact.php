f8c3ad41a21b9600125e373398e5c12ee5c8fefb
Dustin Ruetz <dustinruetz@gmail.com>
2016-06-04 13:23:26 -0400

    Web optimzed images, finshed contact.php, styled contact.php

--- page-contact.php
+++ page-contact.php
@@ -4,42 +4,41 @@
 	<div class="page-title">
 		<h2><?php the_title(); ?></h2>
 	</div>
-	<div class="container">
-		<section class="contact-info">
-			<div class="contact-phone">
-				<p> <?php echo get_field('page_phone'); ?> </p>
-			</div>
-			<div class="contact-address">
-				<p> <?php echo get_field('page_address'); ?> </p>
-			</div>
-			<div class="contact-email">
-				<p> <?php echo get_field('page_email'); ?> </p>
-			</div>
-		</section>
-		<section class="form-map">
-			<div class="form">
-				<?php // Start the loop ?>
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<section class="contact-info">
+		<div class="contact-phone">
+			<i class="fa fa-2x fa-phone"></i>
+			<a href="tel: <?php echo get_field('page_phone'); ?> "> <?php echo get_field('page_phone'); ?> </a>
+		</div>
+		<div class="contact-address">
+			<i class="fa fa-2x fa-map-marker"></i>
+			<p> <?php echo get_field('page_address'); ?> </p>
+		</div>
+		<div class="contact-email">
+			<i class="fa fa-envelope"></i>
+			<a href="mailto: <?php echo get_field('page_email'); ?> "> <?php echo get_field('page_email'); ?> </a>
+		</div>
+	</section>
+	<section class="form-map">
+		<div class="form">
+			<?php // Start the loop ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-					<?php the_content(); ?>
+				<?php the_content(); ?>
 
-				<?php endwhile; // end the loop?>
-			</div>
-			<div class="map">
-				<?php 
-					$location = get_field('google_map');
+			<?php endwhile; // end the loop?>
+		</div>
+		<div class="map">
+			<?php 
+				$location = get_field('google_map');
 
-					if( !empty($location) ):
-					?>
-					<div class="acf-map">
-						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-					</div>
-				<?php endif; ?>
-
-				<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.9924761236693!2d-79.40008148450244!3d43.64832487912135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34dc87fa67bd%3A0xe05376a9e49647e1!2s483+Queen+St+W%2C+Toronto%2C+ON+M5V+2A9!5e0!3m2!1sen!2sca!4v1464901035456" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
-			</div>
-		</section> <!-- /.form-map -->
-	</div> <!-- /.container -->
+				if( !empty($location) ):
+				?>
+				<div class="acf-map">
+					<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+				</div>
+			<?php endif; ?>
+		</div>
+	</section> <!-- /.form-map -->
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
