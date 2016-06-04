a6d2ebc378ccb4ec1bd5e458efb820d8db217ad3
Jordan Keen <jordankeen.7@gmail.com>
2016-06-04 19:49:19 -0400

    about section and quote section

--- category.php
+++ category.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
+<div class="page-title">
+    <h2> <?php echo $our_title = get_the_title( get_option('page_for_posts', true) ); ?> </h2>
+  </div>
+
 <div class="main">
   <div class="container">
     <div class="content">
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php //index.php is the last resort template, if no other templates match ?>
 <?php get_header(); ?>
+<?php //index.php is the last resort template, if no other templates match ?>
 
 <div class="main">
 
